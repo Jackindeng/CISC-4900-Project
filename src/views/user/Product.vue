3b969eaa87c63881b0e7b9d3dda2105390bd5336
Jackindeng <Jackindeng@gmail.com>
2026-02-27 07:49:06 -0500

    Add files via upload

---
+++ src/views/user/Product.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        Product information
+    </div>
+</template>
+<script>
+export default {
+    name: 'Product',
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    methods: {
+    }
+};
+</script>
+<style scoped lang="scss">
+</style>
