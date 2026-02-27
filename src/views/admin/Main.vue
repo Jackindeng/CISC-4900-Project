c5722d16ad804031d88961a764eb3ec2f70f9db6
Jackindeng <Jackindeng@gmail.com>
2026-02-27 07:41:00 -0500

    Add files via upload

---
+++ src/views/admin/Main.vue
@@ -0,0 +1,22 @@
+<template>
+    <div style="padding: 30px;">
+        Dashboard
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    },
+    created() {
+      
+    },
+    methods: {
+       
+    },
+};
+</script>
+<style scoped lang="scss">
+</style>
---
+++ src/views/user/MySave.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        My Save
+    </div>
+</template>
+<script>
+export default {
+    name: 'MySave',
+    data() {
+        return {
+
+        };
+    },
+    created() {
+
+    },
+    methods: {
+    }
+};
+</script>
+<style scoped lang="scss">
+</style>
